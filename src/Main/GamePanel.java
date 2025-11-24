30e7b4a057d90348d517100ed18e6de68349f8ad
Iyah Dadula <alliyahdadula@gmail.com>
2025-11-25 03:05:28 +0800

    First Commit

--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -86,6 +86,7 @@ public class GamePanel extends JPanel implements Runnable {
 
         Graphics2D g2 = (Graphics2D) g;
 
+        tileM.draw(g2);
         player.draw(g2);
 
         g2.dispose(); // disposes graphics context to save memory
