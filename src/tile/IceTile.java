7147bc1ee11cf3ab9ee11a9d83bf4100acfbe596
Iyah Dadula <alliyahdadula@gmail.com>
2025-11-25 22:52:36 +0800

    Fix: IceTile

--- src/tile/IceTile.java
+++ src/tile/IceTile.java
@@ -2,7 +2,7 @@ package tile;
 
 import java.awt.image.BufferedImage;
 
-public class Tile {
+public class IceTile extends Tile {
 
     public BufferedImage image;
     public boolean collision = false;
